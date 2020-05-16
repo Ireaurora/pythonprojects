5aa29fc63297df7eeeff74ee470538e9e584cdbd
Irene Sarigu <ireaurora@gmail.com>
2020-05-16 18:05:48 +0100

    Update dice_rolling_simulator.py

--- dice_rolling_simulator.py
+++ dice_rolling_simulator.py
@@ -2,16 +2,10 @@ from random import randint
 
 question = str(input("Should I roll the dice?"))
 
-def sum():
-    return randint(0, 6) + randint(0, 6)
-
-def roll_the_dice():
-    return sum()
-
-def start_question():
-    while question == "yes":
-        return roll_the_dice()
+def roll():
+    if question == "yes":
+        return "First dice : " +str(randint(0, 6))  + "\nSecond dice : "  + str(randint(0, 6))
     else:
-        return "Okay"
+        return "Okay :("
     
-print(start_question())
+print(roll())
